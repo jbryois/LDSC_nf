a5ffc9f6026779d61ad9b8a4182915855ec1828d
Julien Bryois <julien.bryios@gmail.com>
2019-12-03 16:29:31 +0100

    updated to keep track of original file

--- bin/CleanH2Results.R
+++ bin/CleanH2Results.R
@@ -8,11 +8,11 @@ files <- list.files(".",pattern=".results",full.names = TRUE)
 d <- tibble(filename=files) %>% mutate(file_contents = map(filename,read_tsv)) %>%
   mutate(makenames=gsub(".results","",basename(filename)),
          Trait=sub("_.*","",makenames),
-         Annot=sub("^[^_]*_", "", makenames)) %>% 
+         File=basename(filename)) %>% 
   select(-filename,-makenames) %>%
   unnest(file_contents) %>%
   mutate(Coefficient_P=pnorm(`Coefficient_z-score`,lower.tail=FALSE)) %>%
-  select(Trait,Annot,Category,Prop._SNPs:`Coefficient_z-score`,Coefficient_P) %>%
+  select(File,Trait,Category,Prop._SNPs:`Coefficient_z-score`,Coefficient_P) %>%
   mutate(Category=gsub(".bedL2_0|L2_1|L2_0","",Category)) %>%
   arrange(Trait,Enrichment_p)
   
