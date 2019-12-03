15449168e090fb07a78a4a443df16b1c5b332b67
Julien Bryois <julien.bryios@gmail.com>
2019-12-03 16:12:31 +0100

    updated r script to get annotation

--- bin/CleanH2Results.R
+++ bin/CleanH2Results.R
@@ -7,11 +7,12 @@ files <- list.files(".",pattern=".results",full.names = TRUE)
 
 d <- tibble(filename=files) %>% mutate(file_contents = map(filename,read_tsv)) %>%
   mutate(makenames=gsub(".results","",basename(filename)),
-         Trait=sub("_.*","",makenames)) %>% 
+         Trait=sub("_.*","",makenames),
+         Annot=sub("^[^_]*_", "", makenames)) %>% 
   select(-filename,-makenames) %>%
   unnest(file_contents) %>%
   mutate(Coefficient_P=pnorm(`Coefficient_z-score`,lower.tail=FALSE)) %>%
-  select(Trait,Category,Prop._SNPs:`Coefficient_z-score`,Coefficient_P) %>%
+  select(Trait,Annot,Category,Prop._SNPs:`Coefficient_z-score`,Coefficient_P) %>%
   mutate(Category=gsub(".bedL2_0|L2_1|L2_0","",Category)) %>%
   arrange(Trait,Enrichment_p)
   
